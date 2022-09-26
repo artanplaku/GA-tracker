b315968e7da72fff65165450df5abaec9edbc68e
Artan <artanpl03@gmail.com>
2022-09-26 12:48:02 -0400

    added user model

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,7 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require("mongoose");
+const model = require('./usermodel')
 
 app.listen('4000', () => console.log("Server started on port 4000"))
