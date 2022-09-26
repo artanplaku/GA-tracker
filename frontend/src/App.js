d7812e3cdc23fde60d9e400040710723f196aaf3
Artan <artanpl03@gmail.com>
2022-09-26 12:21:50 -0400

    changes to App, login and register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route} from "react-router-dom"
+import Register from './components/Register';
+import Login from './components/Login';
+
 
 function App() {
   return (
-    <div className="App">
-      
+   <div>
+    <Routes>
+      <Route path = "/" element={<Register/>}/>
+      <Route path = "/login" element={<Login/>}/>
+    </Routes>
     </div>
+   
   );
 }
 
