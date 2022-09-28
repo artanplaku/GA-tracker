6a52a1ef4d5538cb875606384807991a16e44d67
Laura <lauratelqiu@gmail.com>
2022-09-28 11:51:05 -0400

    added state

--- frontend/src/components/NewApplication/ApplicationForm.js
+++ frontend/src/components/NewApplication/ApplicationForm.js
@@ -1,36 +1,61 @@
-import React from 'react';
-import "../UI/ApplicationForm.css"
+import React, { useState } from "react";
+import "../UI/ApplicationForm.css";
 
 function ApplicationForm() {
+  const [enteredDate, setEnteredDate] = useState("");
+  const [enteredCompanyName, setEnteredCompanyName] = useState("");
+  const [enteredposition, setEnteredpositon] = useState("");
+  const [enterednotes, setEnterednotes] = useState("");
+  const [enteredStatus, setEnteredstatus] = useState("");
+
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+  };
+
+  const companyNameChangeHandler = (event) => {
+    setEnteredCompanyName(event.target.value);
+  };
+
+  const positionChangeHandler = (event) => {
+    setEnteredpositon(event.target.value);
+  };
+
+  const notesChangeHandler = (event) => {
+    setEnterednotes(event.target.value);
+  };
+
+  const statusChangeHandler = (event) => {
+    setEnteredstatus(event.target.value);
+  };
   return (
     <form>
-      <div className='new-application__controls'>
-        <div className='new-application__control'>
+      <div className="new-application__controls">
+        <div className="new-application__control">
           <label>Date</label>
-          <input type="date" />
+          <input type="date" onChange={dateChangeHandler} />
         </div>
-        <div className='new-application__control'>
+        <div className="new-application__control">
           <label>Company Name</label>
-          <input type="text" />
+          <input type="text" onChange={companyNameChangeHandler} />
         </div>
-        <div className='new-application__control'>
+        <div className="new-application__control">
           <label>Position</label>
-          <input type="text" />
+          <input type="text" onChange={positionChangeHandler} />
         </div>
-        <div className='new-application__control'>
+        <div className="new-application__control">
           <label>Notes</label>
-          <input type="text" />
+          <input type="text" onChange={notesChangeHandler} />
         </div>
-        <div className='new-application__control'>
+        <div className="new-application__control">
           <label>Status</label>
-          <input type="text" />
+          <input type="text" onChange={statusChangeHandler} />
         </div>
       </div>
-      <div className='new-application__actions'>
+      <div className="new-application__actions">
         <button type="submit">Add Application</button>
       </div>
     </form>
-  )
+  );
 }
 
 export default ApplicationForm;
