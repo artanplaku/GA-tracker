bd639f1caaad047bb4db9ef555ef7fc299e0958f
Artan <artanpl03@gmail.com>
2022-09-29 22:43:29 -0400

    added cors function

--- backend/server.js
+++ backend/server.js
@@ -4,4 +4,9 @@ const cors = require('cors');
 const mongoose = require("mongoose");
 const model = require('./usermodel')
 
+app.use(cors());
+app.use(express.json());
+
+// mongoose.connect
+
 app.listen('4000', () => console.log("Server started on port 4000"))
