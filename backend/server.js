7433224c22e4756428d2f5e7f9219880d8dd2ed6
Artan <artanpl03@gmail.com>
2022-10-17 13:40:02 -0400

    can now get tokens on postman

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
 const mongoose = require("mongoose");
 const userRoutes = require('./routes/user')
+require('dotenv').config()
 
 const app = express();
 
