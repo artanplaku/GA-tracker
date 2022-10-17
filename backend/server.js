0bd448341dabd5cbb63078c0c896248967950a17
Artan <artanpl03@gmail.com>
2022-10-17 11:58:40 -0400

    added controllers, models, routes

--- backend/server.js
+++ backend/server.js
@@ -1,37 +1,26 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
 const mongoose = require("mongoose");
-const model = require('./usermodel')
+const userRoutes = require('./routes/user')
+
+const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method)
+  next()
+})
+
+app.use('/api/user', userRoutes)
 
 let mongooseConfig = {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }
 
-mongoose.connect("mongodb://localhost/tracker",mongooseConfig)
+mongoose.connect("mongodb://localhost/tracker", mongooseConfig)
 
-
-app.get("/", (req, res) => {
-  res.json("hello world")
-})
-
-app.post("/api/register", async (req, res) => {
-   try{
-  const user = await model.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-    });
-    res.json({ status: "ok" })
-   } catch(err){
-
-    res.json ({ status: "Duplicate Email" })
-
-   }
-  });
 
 app.listen('4000', () => console.log("Server started on port 4000"))
